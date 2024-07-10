f1591c2e0ed5bbee1fddb8e13621c2d9c87951c9
Aamna Bibi <53260264+Aamna-Bibi@users.noreply.github.com>
2024-07-10 14:53:02 +0500

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Tu Hai To Dil",
+    artist: "Hunny, Bunny, Sagar",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "A Thousand Years",
+    artist: "Christina Perri",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Pehle Bhi Mein",
+    artist: "Vishal Mishra & Raj Shekhar",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Who Says",
+    artist: "Selena Gomez",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "At My Worst",
+    artist: "Pink Sweets",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Zindagi Tere Naam Ki",
+    artist: "Vishal Mishra & Kaushal Kishore",
+    img: "music-6",
+    src: "music-6"
+  },
+];
